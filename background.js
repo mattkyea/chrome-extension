1a35a20df18e59afd4e47f68fa0ac7122efa2b0c
mattkyea <kyeamatt@gmail.com>
2018-06-05 16:23:42 -0400

    can only right click on youtube pages and the spotify web player (looking for workaround b/c they won't let you right click)

--- background.js
+++ background.js
@@ -1,15 +1,33 @@
+javascript:void(document.oncontextmenu=null);//allows for context menus on spotify web player (disabled by default)
+window.oncontextmenu = null;//FIX
+
 function onClickHandler(info){
     if (info.menuItemId == "chords") {
         alert("lets get some chords");
+        getYoutubeTitle(window.location.href);
     }else{
         alert("lets get some tab");
     }
 };
 
+function getYoutubeTitle(url){
+    alert(url);
+}
+
 chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+var showForPages = ["*://www.youtube.com/*", "*://open.spotify.com/*"];
 
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
-    var chords = chrome.contextMenus.create({"title": "Chords", "id": "chords"});
-    var tab = chrome.contextMenus.create({"title": "Tabs", "id": "tab"});
+    var chords = chrome.contextMenus.create({
+        "title": "Chords", 
+        "id": "chords",
+        "documentUrlPatterns": showForPages
+    });
+    var tab = chrome.contextMenus.create({
+        "title": "Tabs", 
+        "id": "tab",
+        "documentUrlPatterns": showForPages
+    });
 });

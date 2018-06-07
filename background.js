acff1f38d56cca2dc0da9d9294c4962d9fc41a4c
mattkyea <kyeamatt@gmail.com>
2018-06-07 17:37:15 -0400

    added listener for url from contentScript

--- background.js
+++ background.js
@@ -1,21 +1,5 @@
-javascript:void(document.oncontextmenu=null);//allows for context menus on spotify web player (disabled by default)
-window.oncontextmenu = null;//FIX
-
-function onClickHandler(info){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        if(info.menuItemId == "chords"){
-            //alert("lets get some chords");
-            chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_chords"});
-        }else{
-            alert("lets get some tab");
-            chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_tab"});
-        }
-})};
-
-console.log("howdy");
-
-chrome.contextMenus.onClicked.addListener(onClickHandler);
+//javascript:void(document.oncontextmenu=null);//allows for context menus on spotify web player (disabled by default)
+//window.oncontextmenu = null;//FIX
 
 var showForPages = ["*://www.youtube.com/*", "*://open.spotify.com/*"];
 
@@ -32,3 +16,32 @@ chrome.runtime.onInstalled.addListener(function() {
         "documentUrlPatterns": showForPages
     });
 });
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+//what to do when button pressed
+function onClickHandler(info){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        if(info.menuItemId == "chords"){
+            //alert("lets get some chords");
+            //alert(activeTab);
+            //alert(activeTab.title);
+            //alert("come on man");
+            chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_chords"});
+        }else{
+            //alert("lets get some tab");
+            chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_tab"});
+        }
+})};
+
+
+//waiting for new link
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "open_new_tab" ) {
+        //alert("lets open er up");
+        chrome.tabs.create({"url": request.url});
+      }
+    }
+  );

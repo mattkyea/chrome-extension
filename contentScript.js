69a2ecf4932f4b0816cd576e6d4739be9b5a1c00
mattkyea <kyeamatt@gmail.com>
2018-06-08 17:53:04 -0400

    trying to listen for another message

--- contentScript.js
+++ contentScript.js
@@ -9,5 +9,16 @@ chrome.runtime.onMessage.addListener(
         } 
         var url = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + title;
         chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
+        }
+  );
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(request.message === "grab_link"){
+            console.log("wow really");
+            var link = $("a").text();//3rd link gets past two ads
+            console.log(link);
+            chrome.runtime.sendMessage({"message": "open_link", "link": link});
+        }
     }
   );

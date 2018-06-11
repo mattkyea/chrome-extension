c7266e4842fd876599afc2985aa6f645e3582590
mattkyea <kyeamatt@gmail.com>
2018-06-11 17:32:41 -0400

    reworked listening and message sending using ports

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 //javascript:void(document.oncontextmenu=null);//allows for context menus on spotify web player (disabled by default)
 //window.oncontextmenu = null;//FIX
 
+/*
 var showForPages = ["*://www.youtube.com/*", "*://open.spotify.com/*"];
 
 // Set up context menu tree at install time.
@@ -37,20 +38,19 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "open_new_tab" ) {
         chrome.tabs.create({"url": request.url});
-        onOpenHandler();
+        chrome.tabs.query({active: true, currentWindow: true}, function(sites) {
+            var activeSite = sites[0];
+            console.log("good so far");
+            console.log(activeSite.id);
+            chrome.tabs.sendMessage(activeSite.id, {"message": "grab_link"});
+        })
+        }else if(request.message === "open_link"){
+            console.log("no way");
+            chrome.tabs.create({"link": request.link});
         }
     }
 );
-
-function onOpenHandler(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(sites) {
-        var activeSite = sites[0];
-        console.log("good so far");
-        console.log(activeSite.id);
-        chrome.tabs.sendMessage(activeSite.id, {"message": "grab_link"});
-    })
-};
-
+/*
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.message === "open_link"){
@@ -59,3 +59,46 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+*/
+chrome.runtime.onInstalled.addListener(function() {
+    var showForPages = ["*://www.youtube.com/*", "*://open.spotify.com/*", "*://www.ultimate-guitar.com/*"];
+    var chords = chrome.contextMenus.create({
+        "title": "Chords", 
+        "id": "chords",
+        "documentUrlPatterns": showForPages
+    });
+    var tab = chrome.contextMenus.create({
+        "title": "Tabs", 
+        "id": "tab",
+        "documentUrlPatterns": showForPages
+    });
+});
+chrome.contextMenus.onClicked.addListener(
+    function(info){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {            
+            var activeTab = tabs[0];
+            var port = chrome.tabs.connect(activeTab.id, {name: "defaultPort"});
+            if(info.menuItemId == "chords"){
+                console.log("neato");
+                port.postMessage({"message": "clicked_chords"});
+            }else{
+                port.postMessage({"message": "clicked_tab"});
+            }
+            port.onMessage.addListener(function(msg) {
+                if (msg.message == "open_new_tab"){
+                    console.log("sick");
+                    chrome.tabs.create({"url": msg.url});
+                    chrome.tabs.query({active: true, currentWindow: true}, function(sites) {
+                        var activeSite = sites[0];
+                        console.log("good so far");
+                        console.log(activeSite.id);
+                        port.postMessage({"ID": activeSite.Id, "message": "grab_link"});
+                    });
+                }else{
+                    chrome.tabs.create({"url": msg.url});
+                }
+            });
+        });
+    }
+);
+
--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,29 @@
+/*
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        var title = $("h1:first").text();//title of video
-        if(request.message === "clicked_chords") {
-            title = title + " chords";
+        if(request.message === "grab_link"){
+            //console.log("wow really");
+            alert("another link time");
+            var link = $("a").text();//3rd link gets past two ads
+            console.log(link);
+            chrome.runtime.sendMessage({"message": "open_link", "link": link});
         }
         else{
-            title = title + " tab";
-        } 
-        var url = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + title;
-        chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
-        }
+            console.log("grabbin title");
+            //alert("title time");
+            var title = $("h1:first").text();//title of video
+            if(request.message === "clicked_chords") {
+                title = title + " chords";
+            }
+            else{
+                title = title + " tab";
+            } 
+            var url = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + title;
+            chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
+        }    
+    }
   );
-
+/*
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.message === "grab_link"){
@@ -21,4 +33,28 @@ chrome.runtime.onMessage.addListener(
             chrome.runtime.sendMessage({"message": "open_link", "link": link});
         }
     }
-  );
+  ); 
+*/
+chrome.runtime.onConnect.addListener(function(port) {
+    console.assert(port.name == "defaultPort");
+    port.onMessage.addListener(function(msg) {
+        alert(msg.message);
+        if(msg.message == "clicked_chords" || msg.message == "clicked_tab"){
+            alert("come on");
+            var title = $("h1:first").text();//title of video
+            if (msg.message == "clicked_chords"){
+                title = title + " chords";
+            }else if(msg.message == "clicked_tab"){
+                title = title + " tab";
+            }
+            var url = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + title;
+            port.postMessage({"message": "open_new_tab", "url": url});
+        }else{
+            alert("no way");
+            var link = $("a href").text();//3rd link gets past two ads
+            port.postMessage({"message": "open_link", "url": link});
+        }
+    });
+});
+        
+

355b2a2d8030047a95ae2f5ab2864bc6b306dc15
mattkyea <kyeamatt@gmail.com>
2018-06-12 17:49:31 -0400

    working on grabbing link from text of html

--- contentScript.js
+++ contentScript.js
@@ -38,9 +38,9 @@ chrome.runtime.onMessage.addListener(
 chrome.runtime.onConnect.addListener(function(port) {
     console.assert(port.name == "defaultPort");
     port.onMessage.addListener(function(msg) {
-        alert(msg.message);
+        //alert(msg.message);
         if(msg.message == "clicked_chords" || msg.message == "clicked_tab"){
-            alert("come on");
+            //alert("come on");
             var title = $("h1:first").text();//title of video
             if (msg.message == "clicked_chords"){
                 title = title + " chords";
@@ -50,9 +50,21 @@ chrome.runtime.onConnect.addListener(function(port) {
             var url = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + title;
             port.postMessage({"message": "open_new_tab", "url": url});
         }else{
-            alert("no way");
-            var link = $("a href").text();//3rd link gets past two ads
-            port.postMessage({"message": "open_link", "url": link});
+            //alert("no way");
+            //var link = $("a").text();//3rd link gets past two ads
+            //var link = $("a[href^='http']").eq(0).attr("href");
+            var link = $.get(msg.url, "text", function(data){
+                //alert(data);
+                var begin = data.indexOf("<a href=\"https://tabs.ultimate-guitar.com/tab/");
+                //<a href="https://tabs.ultimate-guitar.com/tab/pink_floyd/comfortably_numb_tabs_79177" class="link-primary _1kcZ5"><span><span><b>Comfortably</b></span><span> </span><span><b>Numb</b></span></span></a>
+                //alert(begin);
+                var end = data.indexOf("\" class=\"link-primary _1kcZ5\">");
+                //alert(end);
+                var finalURl = data.substring(begin, end);
+                alert(finalURL);
+            });
+            //alert(link);
+            port.postMessage({"message": "open_link", "url": finalURL});
         }
     });
 });
